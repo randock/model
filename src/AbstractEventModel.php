91b17404112908f793ea4feb7d86349f64071dca
Jop Peters <jop@mastercoding.nl>
2017-08-21 20:37:19 +0200

    Make use of doctrine/common and randock/event

--- src/AbstractEventModel.php
+++ src/AbstractEventModel.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Randock\Model;
 
+use Randock\Event\Definition\DomainEvent;
 use Doctrine\Common\Collections\Collection;
-use Randock\Model\Event\Definition\DomainEvent;
 use Randock\Model\Definition\PatchableInterface;
 use Randock\Model\Exception\NotPatchableException;
 
