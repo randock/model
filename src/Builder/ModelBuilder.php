bd7cdf404b841c9dab2cfd9d2689fbdbeba2f70a
Jop Peters <jop@mastercoding.nl>
2018-04-03 19:53:40 +0200

    Allow sf4

--- src/Builder/ModelBuilder.php
+++ src/Builder/ModelBuilder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Randock\Model\Builder;
 
-use Symfony\Component\Form\Exception\OutOfBoundsException;
 use Symfony\Component\Form\FormInterface;
 
 class ModelBuilder
